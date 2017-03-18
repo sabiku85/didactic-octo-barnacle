47bc43b4edd6a51a6d7d609ca54638f9394b7981
Sabi_k <Sabi_k@sabi.k>
2017-03-18 17:47:03 +0100

    First commit

---
+++ script.js
@@ -0,0 +1,29 @@
+
+	header > nav > div > ul > li {
+		font-size: 1rem;
+		border: 1px solid #FFF;
+		border-radius: 2%;
+		margin: 5% 15%;
+	}
+	
+		header > nav > div > ul > li:last-child {
+		margin-bottom: 15%;
+	}
+	
+	header > nav > div > ul > li > a {
+		color: #FFF;
+		display: block;
+		line-height: 3rem;
+		padding: 2% 10%;
+		text-decoration: none;
+		text-transform: uppercase;	
+	}
+	
+	header > nav > div >ul > li > a:hover {
+		background: #454746;
+		color: #05BAF3;
+	}
+	
+	header > nav > div > img {
+		margin-top: 5%;
+	}
