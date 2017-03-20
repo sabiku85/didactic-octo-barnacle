93ba849de793fdab70736549500515351a52d429
Sabi_k <Sabi_k@sabi.k>
2017-03-20 21:22:07 +0100

    Adding Gruntfile.js, package.json, node_modules/

---
+++ Gruntfile.js
@@ -0,0 +1,60 @@
+module.exports = function(grunt) {
+
+		// Project configuration.
+	grunt.initConfig({
+  		sass: {
+	    	options: {
+	       		sourceMap: true
+	    	},
+	   		dist: {
+	   			files: {
+	       			'css/main.css': 'sass/main.sass'
+	       		}
+    		}
+		},
+		
+		imagemin: {
+			dynamic: {
+				files: [{
+					expand: true,
+					cwd: 'images/',
+					src: ['**/*.{png,jpg,gif}'],
+					dest: 'images/build/'
+				}]
+			}
+		},
+
+		watch: {
+			scripts: {
+				files: ['sass/*.sass'],
+				tasks: ['sass'],
+				options: {
+					spawn: false,
+				}
+			}
+		},
+
+		browserSync: {
+			bsFiles: {
+				src: 'assets/css/*.css'
+			},
+			options: {
+				watchTask: true,
+				server: {
+					baseDir: "./"
+				}
+			}
+		}
+	});	
+	
+		// Load the plugins tasks 
+	grunt.loadNpmTasks('grunt-sass');
+	grunt.loadNpmTasks('grunt-contrib-imagemin');
+	grunt.loadNpmTasks('grunt-contrib-watch');
+	grunt.loadNpmTasks('grunt-browser-sync');
+
+			// Default task(s).
+
+	grunt.registerTask('default', ['sass', 'imagemin', 'browserSync', 'watch']);
+};
+
